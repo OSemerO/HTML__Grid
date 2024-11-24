36538f4ebbf3ad20aab45e98ec2ea98b007b632a
O_Semernina <semernina-olga@mail.ru>
2024-11-24 18:47:36 +0300

    first commit, practicing BEM

--- js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const menuBtn = document.querySelector(".menu-btn");
-
-const menu = document.querySelector(".menu");
-
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("menu--active");
-});
